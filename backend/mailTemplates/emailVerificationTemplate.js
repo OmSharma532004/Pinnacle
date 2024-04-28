6f846d46904ce61405fb46952f70802c3f472076
OmSharma532004 <22103203@mail.jiit.ac.in>
2024-04-28 14:07:48 +0530

    Email Templates Done

--- backend/mailTemplates/emailVerificationTemplate.js
+++ backend/mailTemplates/emailVerificationTemplate.js
@@ -1,3 +1,4 @@
+// const logo=require('../utils/logo.png');
 const otpTemplate = (otp) => {
 	return `<!DOCTYPE html>
 	<html>
@@ -59,6 +60,7 @@ const otpTemplate = (otp) => {
 	
 			.highlight {
 				font-weight: bold;
+				bgcolor: #FFD60A;
 			}
 		</style>
 	
@@ -66,19 +68,18 @@ const otpTemplate = (otp) => {
 	
 	<body>
 		<div class="container">
-			<a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-					src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+		<img src="https://res.cloudinary.com/dtxejnbnw/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1714292978/Image_28-04-24_at_1.58_PM_vszmhw.jpg" alt="PinnacleSolution Logo" class="logo">
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
-				<p>Thank you for registering with StudyNotion. To complete your registration, please use the following OTP
+				<p>Thank you for registering with Pinnacle Solution To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
 				<h2 class="highlight">${otp}</h2>
 				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
 				Once your account is verified, you will have access to our platform and its features.</p>
 			</div>
 			<div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-					href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+					href="mailto:info@PinnacleSolution.com">info@PinnacleSolution.com</a>. We are here to help!</div>
 		</div>
 	</body>
 	
