c6cc139e3bbbf0c0748e378db1f5965c459f6fe9
OmSharma532004 <22103203@mail.jiit.ac.in>
2024-06-05 00:17:53 +0530

    Admin Done

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from '../../user/src/reducer/index.jsx'
+import rootReducer from './redux/index.jsx'
 import { Provider } from 'react-redux'
 import { Toaster } from "react-hot-toast";
 
